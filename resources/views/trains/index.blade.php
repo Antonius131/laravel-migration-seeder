667001144cb96c4eab4add1ddd3f4e9cd85dbb49
Antonius131 <antomendetta@gmail.com>
2022-09-09 18:46:24 +0200

    iteration to sho train table

--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.main')
 
 @section('main-content')
-   <h2>Content goes here</h2>
-@endsection
+   <h2>All trains:</h2>
+
+   <ul>
+      @forelse ($trains as $train)
+         <li>{{ $train->company }} - {{ $train->departure_station }} - {{ $train->arrival_station }} - {{ $train->departure_time }} - {{ $train->arrival_time }} - {{ $train->train_code }} - {{ $train->carriages }}</li>
+      @empty
+         <h2>Nessun treno disponibile</h2>
+      @endforelse
+   </ul>
+@endsection   
