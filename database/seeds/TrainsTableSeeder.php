11cf14f7dbc75286ffe62555e0554d2bb4b7c6e6
Antonius131 <antomendetta@gmail.com>
2022-09-09 16:55:29 +0200

    populating seeder with data

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -10,7 +10,41 @@ class TrainsTableSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        //
+    { 
+        $trains = [
+            [
+                'company' => 'Trenitalia',
+                'departure_station' => 'Torino Centrale',
+                'arrival_station' => 'Firenze S. Maria Novella',
+                'departure_time' => '15:20',
+                'arrival_time' => '18:45',
+                'train_code' => '123HTU64DD8',
+                'carriages' => 12,
+                'in_time' => true,
+                'canceled' => false,
+            ],
+            [
+                'company' => 'Italo',
+                'departure_station' => 'Roma Centrale',
+                'arrival_station' => 'Milano Centrale',
+                'departure_time' => '6:05',
+                'arrival_time' => '10:45',
+                'train_code' => '123HUT94DD8',
+                'carriages' => 10,
+                'in_time' => true,
+                'canceled' => false,
+            ],
+            [
+                'company' => 'Trenitalia',
+                'departure_station' => 'Milano Centrale',
+                'arrival_station' => 'Torino Centrale',
+                'departure_time' => '18:40',
+                'arrival_time' => '19:45',
+                'train_code' => '100FTT64DD8',
+                'carriages' => 11,
+                'in_time' => true,
+                'canceled' => false,
+            ]
+        ];
     }
 }
